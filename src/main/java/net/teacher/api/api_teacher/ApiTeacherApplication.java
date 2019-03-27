e14c8dbf20dead6ff11fb564d4480da32a3178ed
lexuanchinh97 <1510305@hcmut.edu.vn>
2019-03-27 21:37:56 +0700

    init

---
+++ src/main/java/net/teacher/api/api_teacher/ApiTeacherApplication.java
@@ -0,0 +1,13 @@
+package net.teacher.api.api_teacher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTeacherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTeacherApplication.class, args);
+	}
+
+}
